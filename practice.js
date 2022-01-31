14662307eda348de204e89b823471dc9f6220033
Zaldana <rooben.zaldana@gmail.com>
2022-01-31 10:30:51 -0500

    async asignment

--- practice.js
+++ practice.js
@@ -10,7 +10,16 @@ const fetchData = require('./fetchDataLibrary')
 // 10. Call fetchData (which returns a promise) and use the .then()  method to log the value the promise resolves with to the javascript console.
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+const consumePromise = fetchData().then(
+    data => console.log(data),
+    error => console.log(error)
+)
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 11. Call fetchData (which returns a promise) and use the async/await method to log the value the promise resolves with to the javascript console.
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+async function asyncFunc() {
+    const result = await fetchData();
+    console.log(result);
+}
